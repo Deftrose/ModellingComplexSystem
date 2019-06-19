7deb99e257ac5d66b71c5c423a77c737a3d6cecf
BOWEN BAI <bbb@student.unimelb.edu.au>
2019-06-19 13:38:41 +1000

    exam

---
+++ Go/Untitled-1.go
@@ -0,0 +1,44 @@
+package main
+
+const N = 20
+
+func producer(out chan<- int) {
+	for i := 0; i < N; i++ {
+		out <- i
+	}
+}
+func adder(in <-chan int, out chan<- int, val int) {
+	for {
+		x := <-in
+		out <- x + val
+	}
+}
+func multiplier(in <-chan int, out chan<- int, val int) {
+	for {
+		x := <-in
+		out <- x * val
+	}
+}
+func consumer(in <-chan int, done chan<- bool, val int) {
+	for i := 0; i < N; i++ {
+		x := <-in
+		if i%val == 0 {
+			print(x, "\n")
+		}
+	}
+	done <- true
+}
+func main() {
+	add_val := 3
+	mult_val := 2
+	filter_val := 3
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+	done := make(chan bool)
+	go producer(ch1)
+	go adder(ch1, ch2, add_val)
+	go multiplier(ch2, ch3, mult_val)
+	go consumer(ch3, done, filter_val)
+	<-done
+}
